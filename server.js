c5722cbf0babfedf7e9fe59a505dadc0c78482bd
Edward Gemar <edwardgemar@Edwards-MacBook-Pro.local>
2017-06-16 15:26:04 -0500

    Updated deployment

--- server.js
+++ server.js
@@ -18,10 +18,28 @@ console.log('Running on http://localhost:' + PORT);
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser'); 
 var port = 8080;
+
+
+//support parsing of application/json type post data
+app.use(bodyParser.text({type: '*/*'}));
+
+//support parsing of application/x-www-form-urlencoded post data
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res){
+  const body = req.body;
+  io.emit('chat message', 'Received post: ' + body);
+  res.set('Content-Type', 'text/plain');
+  res.send('You sent foo: ' + body + ' to Express');
+  //io.emit('chat message', 'Received post: ' + body);
 });
 
 io.on('connection', function(socket){
