c52544fdbdba6f66202f44492b8b8a99afe3dd07
Edward Gemar <edwardgemar@Edwards-MacBook-Pro.local>
2017-06-20 23:43:48 -0500

    Debug updates

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port = 8080;
 
 
 //support parsing of application/json type post data
-//app.use(bodyParser.text({type: '*/*'}));
+app.use(bodyParser.text({type: '*/*'}));
 //bodyParser.json();
 /*
 app.use(bodyParser.urlencoded({
@@ -18,7 +18,10 @@ app.use(bodyParser.urlencoded({
 /**bodyParser.json(options)
  * Parses the text as JSON and exposes the resulting object on req.body.
  */
-app.use(bodyParser.json());
+
+
+// JUST NEED THIS FOR JSON
+//app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 //app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,10 +49,10 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   const body = req.body;
   io.emit(common.DEBUG_CHANNEL_NAME, 'Received post: foo2 ' + body);
-  io.emit(common.DEBUG_CHANNEL_NAME, 'Received post: bar ' + body.event.name);
+  //io.emit(common.DEBUG_CHANNEL_NAME, 'Received post: bar ' + body.event.name);
   res.set('Content-Type', 'text/plain');
   //res.send('You sent foo2: ' + body + ' to Express');
-  res.send('You sent foo2: ' + req.body.event.name + ' to Express');
+  res.send('You sent foo2: ' + body + ' to Express');
   //io.emit('chat message', 'Received post: ' + body);
   
 });
